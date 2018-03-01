c16f1bccaa7249e1090cae96977ed4690a4f441e
Sofie Westerdahl <algotson@gmail.com>
2018-03-01 16:43:27 +0100

    Switch to next level

--- src/Movement.java
+++ src/Movement.java
@@ -5,15 +5,16 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 public class Movement {
 
-    public static void moveGameObject(Terminal terminal, Player player, Monster monster, char[][] array) {
+//    public static void moveGameObject(Terminal terminal, Player player, Monster monster, char[][] array) {
+//
+//        movePlayer(terminal, player, array);
+//        moveMonster(player, monster, array);
+//
+//    }
 
-        movePlayer(terminal, player, array);
-        moveMonster(player, monster, array);
-
-    }
-
-    public static void movePlayer(Terminal terminal, Player player, char[][] array) {
+    public static boolean movePlayer(Terminal terminal, Player player, char[][] array) {
         Key key;
+        boolean nextlevel = false;
         do {
             try {
                 Thread.sleep(5);
@@ -28,32 +29,33 @@ public class Movement {
             case ArrowDown:
                 if (player.y < Board.BOARD_HEIGHT && (array[player.x][player.y + 1]) != 'x') {
                     if (array[player.x][player.y + 1] == 'E')
-                        System.exit(0);
+                        nextlevel = true;
                     player.y++;
                 }
                 break;
             case ArrowUp:
                 if (player.y > 0 && (array[player.x][player.y - 1]) != 'x') {
                     if (array[player.x][player.y - 1] == 'E')
-                        System.exit(0);
+                        nextlevel = true;
                     player.y--;
                 }
                 break;
             case ArrowLeft:
                 if (player.x > 0 && (array[player.x - 1][player.y]) != 'x') {
                     if (array[player.x - 1][player.y] == 'E')
-                        System.exit(0);
+                        nextlevel = true;
                     player.x--;
                 }
                 break;
             case ArrowRight:
                 if (player.x < Board.BOARD_WIDTH && (array[player.x + 1][player.y]) != 'x') {
                     if (array[player.x + 1][player.y] == 'E')
-                        System.exit(0);
+                        nextlevel = true;
                     player.x++;
                 }
                 break;
         }
+        return nextlevel;
     }
 
     public static void moveMonster(Player player, Monster monster, char[][] array) {
--- src/RunGame.java
+++ src/RunGame.java
@@ -3,64 +3,62 @@
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.terminal.Terminal;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.nio.charset.Charset;
+import java.util.Scanner;
 
 public class RunGame {
-    public static void rungame() {
+    public static void rungame(){
         Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
         terminal.enterPrivateMode();
         terminal.setCursorVisible(false);
 
-
-        File file = null;
-        BufferedReader read = null;
-        File newfile = null;
-        BufferedReader read2 = null;
-
+        Scanner scanNewLevel = null;
 
         Player player = new Player();
         Monster monster = new Monster();
 
         char[][] levelArray = new char[30][30];
 
-        while (true) {
+        for (int k = 1; k <= 5; k++) {
+            File newfile = new File("Levels\\Board" + k + ".txt");
+            try {
+                scanNewLevel = new Scanner(new FileReader(newfile));
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
 
             // läs in en ny bana
-
-            try {
-                file = new File("Levels\\LevelCollection.txt");
-                read = new BufferedReader(new FileReader(file));
-                String level = read.readLine();
-                newfile = new File("Levels\\" + level);
-                read2 = new BufferedReader(new FileReader(newfile));
-
-                for (int i = 0; i < Board.BOARD_HEIGHT; i++) {
-                    char[] line = read2.readLine().toCharArray();
-                    for (int j = 0; j < Board.BOARD_WIDTH; j++) {
-                        levelArray[i][j] = line[j];
-                    }
+            for (int i = 0; i < Board.BOARD_HEIGHT; i++) {
+                char[] line = scanNewLevel.nextLine().toCharArray();
+                for (int j = 0; j < Board.BOARD_WIDTH; j++) {
+                    levelArray[i][j] = line[j];
                 }
-            } catch (FileNotFoundException e1) {
-                e1.printStackTrace();
-            } catch (Exception e2) {
-                e2.printStackTrace();
             }
+
+            // player and monster start position, randomize
+            player.x = 28;
+            player.y = 2;
+            monster.x = 2;
+            monster.y = 28;
 
             //kör bana tills det blir game over, vilket kontrolleras och hanteras i GameOver
             do {
                 Draw.drawBoardAndGameObjects(terminal, player, monster, levelArray);
-                Movement.moveGameObject(terminal, player, monster, levelArray);
+                if (Movement.movePlayer(terminal, player, levelArray))
+                    break;
+                Movement.moveMonster(player, monster, levelArray);
             } while (GameOver.isPlayerAlive(terminal, player, monster));
 
 
-            if (GameOver.continuePlaying(terminal))
-                continue;
-            else
-                System.exit(0);
+//            if (GameOver.continuePlaying(terminal))
+//                continue;
+//            else
+//                System.exit(0);
+
         }
     }
 
